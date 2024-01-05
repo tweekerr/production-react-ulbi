0e7a2772d8dfb1bd6c75c9b393dba6ec7f90d113
tweekerr <gektweeker@gmail.com>
2024-01-06 00:16:53 +0200

    Added jest, added first test

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,26 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+  test('one class', () => {
+    expect(classNames('testClass')).toBe('testClass');
+  });
+
+  test('with additional', () => {
+    expect(classNames('testClass', {}, ['123'])).toBe('testClass 123');
+  });
+
+  test('with mods', () => {
+    const expected = 'testClass 123 hover';
+    expect(classNames('testClass', { hover: true }, ['123'])).toBe(expected);
+  });
+
+  test('with false mod', () => {
+    const expected = 'testClass 123';
+    expect(classNames('testClass', { hover: false }, ['123'])).toBe(expected);
+  });
+
+  test('with undefined mod', () => {
+    const expected = 'testClass 123';
+    expect(classNames('testClass', { hover: undefined }, ['123'])).toBe(expected);
+  });
+});
